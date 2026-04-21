c2badbb2df7ec67659981f488ccafedc57b523b6
Vhierman <achmadfirmansyah989@yahoo.co.id>
2026-04-21 08:52:53 +0700

    Add Model

---
+++ app/Models/Admin/Employees.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employees extends Model
+{
+    //
+}
---
+++ app/Models/Admin/QccInternals.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class QccInternals extends Model
+{
+    //
+}
